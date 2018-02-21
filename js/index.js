e1de07b1249f7e39e8a9a8c50c8e156391822f3f
Simon Garnotel <simon.garnotel@gmail.com>
2018-02-21 10:36:28 +0100

    add citation

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
+// Slides
 var play = 1;
 var slideIndex = 0;
 var slideTimer;
 
 function showSlides() {
 	var i;
-	var slides = document.getElementsByClassName("slidesImages");
-	var dots = document.getElementsByClassName("slidesDot");
+	var slides = document.getElementsByClassName('slidesImages');
+	var dots = document.getElementsByClassName('slidesDot');
 	for (i = 0; i < slides.length; i++)
-		slides[i].style.display = "none";
+		slides[i].style.display = 'none';
 	slideIndex++;
 	if (slideIndex > slides.length)
 		slideIndex = 1;
 	for (i = 0; i < dots.length; i++)
-		dots[i].className = dots[i].className.replace(" active", "");
-	slides[slideIndex-1].style.display = "flex";
-	dots[slideIndex-1].className += " active";
+		dots[i].className = dots[i].className.replace(' active', '');
+	slides[slideIndex-1].style.display = 'flex';
+	dots[slideIndex-1].className += ' active';
 	if (play === 1)
 		slideTimer = setTimeout(showSlides, 5000);
 }
@@ -29,18 +30,33 @@ function playPause() {
 	var button = document.getElementById('slidesPlayPause');
 	
 	if (play === 1) {
-		button.style.backgroundImage = 'url("img/slides/play.png")';
+		button.style.backgroundImage = 'url("img/icons/play.png")';
 		play = 0;
 		clearTimeout(slideTimer);
 	}
 	else {
-		button.style.backgroundImage = 'url("img/slides/pause.png")';
+		button.style.backgroundImage = 'url("img/icons/pause.png")';
 		play = 1;
 		showSlides();
 	}
 }
 
-// resize x3d
+// Cite
+var cite = 0;
+function showCite() {
+	if (cite === 0) {
+		var citetext = document.getElementById('citetext');
+		citetext.style.display = 'block';
+		cite = 1;
+	}
+	else {
+		var citetext = document.getElementById('citetext');
+		citetext.style.display = 'none';
+		cite = 0;
+	}
+}
+
+// X3d
 function resizeX3d() {
 	var i;
 	var x3ds = document.getElementsByTagName('x3d');
