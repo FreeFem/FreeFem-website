02303a9af3482dec8e471c6149d808083fa81d6e
Simon Garnotel <simon.garnotel@gmail.com>
2018-02-21 13:55:49 +0100

    fix img height bug

--- js/index.js
+++ js/index.js
@@ -1,31 +1,49 @@
 // Slides
 var play = 1;
-var slideIndex = 0;
+var slideIndex = 1;
 var slideTimer;
 
+//hide all slides
+function hideSlides() {
+	var slides = document.getElementsByClassName('slidesImages');
+	for (var i = 0; i < slides.length; i++)
+		slides[i].style.display = 'none';
+}
+
+//show all slides
 function showSlides() {
+	var slides = document.getElementsByClassName('slidesImages');
+	for (var i = 0; i < slides.length; i++)
+		slides[i].style.display = 'flex';
+}
+
+//show slide one by one
+function showOneSlide(plus=1) {
 	var i;
 	var slides = document.getElementsByClassName('slidesImages');
 	var dots = document.getElementsByClassName('slidesDot');
 	for (i = 0; i < slides.length; i++)
 		slides[i].style.display = 'none';
-	slideIndex++;
+	if(plus) slideIndex++;
 	if (slideIndex > slides.length)
 		slideIndex = 1;
 	for (i = 0; i < dots.length; i++)
 		dots[i].className = dots[i].className.replace(' active', '');
 	slides[slideIndex-1].style.display = 'flex';
 	dots[slideIndex-1].className += ' active';
-	if (play === 1)
-		slideTimer = setTimeout(showSlides, 5000);
+	if (play === 1){
+		slideTimer = setTimeout(showOneSlide, 5000);
+	}
 }
 
+//change slide
 function changeSlides(i) {
 	slideIndex = i-1;
 	clearTimeout(slideTimer);
-	showSlides();
+	showOneSlide();
 }
 
+//activate play/pause
 function playPause() {
 	var button = document.getElementById('slidesPlayPause');
 	
@@ -37,12 +55,13 @@ function playPause() {
 	else {
 		button.style.backgroundImage = 'url("img/icons/pause.png")';
 		play = 1;
-		showSlides();
+		showOneSlide();
 	}
 }
 
 // Cite
 var cite = 0;
+//show bibtex
 function showCite() {
 	if (cite === 0) {
 		var citetext = document.getElementById('citetext');
@@ -56,25 +75,51 @@ function showCite() {
 	}
 }
 
-// X3d
-function resizeX3d() {
-	var i;
-	var x3ds = document.getElementsByTagName('x3d');
-	var up = document.getElementById('left');
-	var right = document.getElementById('right');
-	
-	for (i = 0; i < x3ds.length; i++){
-		x3ds[i].style.height = 0.5*right.offsetHeight;
-		x3ds[i].style.width = 0.5*left.offsetWidth;
+// Resize
+//resize slides
+function resizeSlides() {
+	var imgRatio = 0.8;
+	// hide all slides
+	hideSlides();
+	// to get the correct width & height of the up container
+	var left = document.getElementById('left');
+	var leftWidth = left.offsetWidth;
+	var leftHeight = left.offsetHeight;
+	// get the down height
+	var down = document.getElementById('down');
+	var downHeight = down.offsetHeight;
+	// show all slides
+	showSlides();
+	// to get the height of the descriptions and compute the correct size of one slide
+	var slides = document.getElementsByClassName('slidesImages');
+	for (var i = 0; i < slides.length; i++) {
+		// description height
+		var descriptionHeight = slides[i].children[1].offsetHeight;
+		// slide width & height
+		slides[i].children[0].style.maxWidth = (leftWidth) + "px";
+		slides[i].children[0].style.maxHeight = (leftHeight - descriptionHeight - downHeight) + "px";
+		// force img or x3d
+		if (slides[i].children[0].children.length === 1) {	//this is an image
+			slides[i].children[0].children[0].style.maxWidth = imgRatio*(leftWidth) + "px";
+			slides[i].children[0].children[0].style.maxHeight = imgRatio*(leftHeight - descriptionHeight - downHeight) + "px";
+		}
+		else if (slides[i].children[0].children.length === 2) { //this is an x3d container
+			var imgHeight = slides[i].children[0].children[0].offsetHeight;
+			slides[i].children[0].children[1].style.width = imgRatio*(leftWidth) + "px";
+			slides[i].children[0].children[1].style.height = imgRatio*(leftHeight - descriptionHeight - downHeight - imgHeight) + "px";
+		}
 	}
+	// begin (or continue) show slide
+	if (slideTimer)
+		clearTimeout(slideTimer);
+	showOneSlide(0);
 }
 
 
 
 // Start
-showSlides();
-resizeX3d();
-window.onresize = resizeX3d;
+resizeSlides();
+window.onresize = resizeSlides;
 
 
 
