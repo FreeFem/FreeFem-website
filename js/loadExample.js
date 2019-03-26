ab6937e4c5b46de15141f5eaffaff2323c62b33c
Simon Garnotel <simon.garnotel@gmail.com>
2019-03-26 14:10:18 +0100

    run examples

---
+++ js/loadExample.js
@@ -0,0 +1,20 @@
+loadExample = (path, editor) => {
+	console.log(path)
+	console.log(editor)
+
+	const xhr = new XMLHttpRequest()
+	xhr.open('GET', path, true)
+	xhr.onload = function(e) {
+		 if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					 editor.setValue(xhr.responseText)
+				} else {
+					 console.error(xhr.statusText)
+				}
+		 }
+	}
+	xhr.onerror = function(e) {
+		 console.error(xhr.statusText)
+	}
+	xhr.send(null)
+}
